e7c98103ce3c2814717c2ceb21841c1731cd872d
SadLaboka <jiutbuh@gmail.com>
2022-12-12 18:54:28 +0300

    add models and base views

--- store/products/models.py
+++ store/products/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    description = models.TextField(null=True, blank=True)
+    slug = models.SlugField(verbose_name='Url', max_length=128, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=0)
+    image = models.ImageField(upload_to='products_images')
+    category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name} | Категория: {self.category}'
+
+    class Meta:
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
--- store/products/views.py
+++ store/products/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 
+from products.models import ProductCategory, Product
+
 def index(request):
-    return render(request, 'products/index.html')
+    context = {'title': 'Store'}
+    return render(request, 'products/index.html', context)
 
 
 def products(request):
-    return render(request, 'products/products.html')
+    context = {'title': 'Store - Каталог',
+               'products': Product.objects.all(),
+               'categories': ProductCategory.objects.all(),
+               }
+    return render(request, 'products/products.html', context)
