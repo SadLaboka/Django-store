3c157c979f949878fd3c6f0038d2f4181f3768ba
sadlaboka <jiutbuh@gmail.com>
2022-12-20 21:03:56 +0300

    add tests for the main views

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,67 @@
-# from django.test import TestCase
+from http import HTTPStatus
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+
+from products.models import Product, ProductCategory
+
+
+class IndexViewTestCase(TestCase):
+
+    def test_view(self):
+        path = reverse('index')
+        response = self.client.get(path)
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.context_data['title'], 'Store')
+        self.assertTemplateUsed(response, 'products/index.html')
+
+
+class ProductsListViewTestCase(TestCase):
+    fixtures = ['categories.json', 'goods.json']
+
+    def setUp(self):
+        self.products = Product.objects.all()
+        self.category = ProductCategory.objects.first()
+
+    def test_list(self):
+        path = reverse('products:index')
+        response = self.client.get(path)
+        self._common_tests(response)
+
+        self.assertEqual(list(response.context_data['object_list']), list(self.products[:1]))
+
+    def test_list_with_category(self):
+        path = reverse('products:category', kwargs={'category_id': self.category.id})
+        response = self.client.get(path)
+        self._common_tests(response)
+
+        self.assertEqual(
+            list(response.context_data['object_list']),
+            list(self.products.filter(category_id=self.category.id)[:1])
+        )
+
+    def test_list_with_page(self):
+        path = reverse('products:paginator', kwargs={'page': 2})
+        response = self.client.get(path)
+        self._common_tests(response)
+
+        self.assertEqual(list(response.context_data['object_list']), list(self.products[1:2]))
+
+    def test_list_with_category_and_page(self):
+        path = reverse(
+            'products:category_paginator',
+            kwargs={'category_id': self.category.id, 'page': 2}
+        )
+        response = self.client.get(path)
+        self._common_tests(response)
+
+        self.assertEqual(
+            list(response.context_data['object_list']),
+            list(self.products.filter(category_id=self.category.id)[1:2])
+        )
+
+    def _common_tests(self, response):
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.context_data['title'], 'Store - Каталог')
+        self.assertTemplateUsed(response, 'products/products.html')
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,97 @@
-# from django.test import TestCase
+from datetime import timedelta
+from http import HTTPStatus
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+from django.utils.timezone import now
+
+from users.models import EmailVerification, User
+
+
+class UserRegistrationViewTestCase(TestCase):
+
+    def setUp(self) -> None:
+        self.path = reverse('users:registration')
+        self.data = {
+            'first_name': 'test_fn',
+            'last_name': 'test_ln',
+            'username': 'testusername',
+            'email': 'test@mail.ru',
+            'password1': 'testPassword1',
+            'password2': 'testPassword1'
+        }
+
+    def test_user_registration_get(self):
+        response = self.client.get(self.path)
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.context_data['title'], 'Store - Регистрация')
+        self.assertTemplateUsed(response, 'users/register.html')
+
+    def test_user_registration_post_success(self):
+        username = self.data['username']
+        self.assertFalse(User.objects.filter(username=username).exists())
+        response = self.client.post(self.path, self.data)
+
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertRedirects(response, reverse('users:login'))
+        self.assertTrue(User.objects.filter(username=username).exists())
+
+        email_verification = EmailVerification.objects.filter(user__username=username)
+        self.assertTrue(email_verification.exists())
+        self.assertEqual(
+            email_verification.first().expiration.date(),
+            (now() + timedelta(hours=48)).date()
+        )
+
+    def test_user_registration_post_error(self):
+        username = self.data['username']
+        User.objects.create(username=username)
+        response = self.client.post(self.path, self.data)
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertContains(response, 'Пользователь с таким именем уже существует.', html=True)
+
+
+class UserLoginViewTestCase(TestCase):
+
+    def setUp(self) -> None:
+        self.path = reverse('users:login')
+        self.user = {
+            'first_name': 'test_fn',
+            'last_name': 'test_ln',
+            'username': 'testusername',
+            'email': 'test@mail.ru'
+        }
+        user = User.objects.create(**self.user)
+        user.set_password('TestPassword1')
+        user.save()
+        self.data = {
+            'username': self.user['username'],
+            'password': 'TestPassword1',
+        }
+
+    def test_user_login_get(self):
+        response = self.client.get(self.path)
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.context_data['title'], 'Store - Авторизация')
+        self.assertTemplateUsed(response, 'users/login.html')
+
+    def test_user_login_post_success(self):
+        response_before = self.client.get(reverse('index'))
+        self.assertFalse(response_before.context['user'].is_authenticated)
+        response = self.client.post(self.path, {'username': 'testusername', 'password': 'TestPassword1'})
+
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertRedirects(response, reverse('index'))
+        self.assertTrue(response.wsgi_request.user.is_authenticated)
+
+    def test_user_login_post_error(self):
+        response_before = self.client.get(reverse('index'))
+        self.assertFalse(response_before.context['user'].is_authenticated)
+        response = self.client.post(self.path, {'username': 'testusername', 'password': 'WrongPass'})
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertTemplateUsed(response, 'users/login.html')
+        self.assertFalse(response.context['user'].is_authenticated)
